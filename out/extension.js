e2a352800e77b3855cded9e6d43f0fc0e63f143d
Yi Ming Yung <yiming.yung@outlook.com>
2025-01-16 16:12:33 +0100

    feat: Improve pattern matching

--- out/extension.js
+++ out/extension.js
@@ -8,44 +8,54 @@ const vscode = require("vscode");
 function activate(context) {
     let disposable = vscode.commands.registerCommand('pytest-grabber.generatePath', () => {
         const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const config = vscode.workspace.getConfiguration('pytest-grabber');
-            const separator = config.get('separator') || '${separator}';
-            const prefixCmd = config.get('prefixCmd') || '';
-            const document = editor.document;
-            const selection = editor.selection;
-            const line = document.lineAt(selection.active.line);
-            let classNameMatch = null;
-            let methodNameMatch = null;
-            // Search for the class and method names above the current line
-            for (let i = selection.active.line; i >= 0; i--) {
-                const line = document.lineAt(i);
-                if (!methodNameMatch) {
-                    methodNameMatch = line.text.match(/def\s+(\w+)\(/);
-                }
-                if (!classNameMatch) {
-                    classNameMatch = line.text.match(/class\s+(\w+)(\()?/);
-                }
-                if (classNameMatch) {
-                    break;
-                }
+        if (!editor)
+            return; // Early return if no editor is active
+        const config = vscode.workspace.getConfiguration('pytest-grabber');
+        const document = editor.document;
+        const selection = editor.selection;
+        let classNameMatch = null;
+        let methodNameMatch = null;
+        // Search for the class and method names above the current line
+        for (let i = selection.active.line; i >= 0; i--) {
+            const line = document.lineAt(i);
+            if (!methodNameMatch)
+                methodNameMatch = line.text.match(/def\s+(\w+)\(/);
+            if (!classNameMatch)
+                classNameMatch = line.text.match(/class\s+(\w+)([\(:])/);
+            if (classNameMatch)
+                break;
+        }
+        // Gather all path components.
+        const separator = config.get('separator') || '${separator}';
+        // Get prefixCmd and add a space if it doesn't have it.
+        let prefixCmd = config.get('prefixCmd') || '';
+        prefixCmd = prefixCmd.endsWith(" ") ? prefixCmd : prefixCmd + " ";
+        const filePath = vscode.workspace.asRelativePath(document.uri, false);
+        const className = classNameMatch?.[1];
+        const methodName = methodNameMatch?.[1];
+        if (!className && !methodName) {
+            vscode.window.showInformationMessage('No match found!');
+        }
+        else {
+            // We have at least one part.
+            // Gather all path components in an array for simplicity
+            const pathComponents = [];
+            pathComponents.push(filePath);
+            if (className && methodName) {
+                pathComponents.push(className, methodName);
             }
-            if (classNameMatch && methodNameMatch) {
-                const className = classNameMatch[1];
-                const methodName = methodNameMatch[1];
-                const filePath = vscode.workspace.asRelativePath(document.uri, false);
-                vscode.env.clipboard.writeText(`${prefixCmd}${filePath}${separator}${className}${separator}${methodName}`);
-                vscode.window.showInformationMessage('Path copied to clipboard');
+            else if (className) {
+                pathComponents.push(className);
             }
-            else if (classNameMatch) {
-                const className = classNameMatch[1];
-                const filePath = vscode.workspace.asRelativePath(document.uri, false);
-                vscode.env.clipboard.writeText(`${prefixCmd}${filePath}${separator}${className}`);
-                vscode.window.showInformationMessage('Path copied to clipboard');
+            else if (methodName) {
+                pathComponents.push(methodName);
             }
-            else {
-                vscode.window.showInformationMessage('No match found!');
-            }
+            let clipboard = "";
+            if (prefixCmd)
+                clipboard += prefixCmd + " ";
+            clipboard += pathComponents.join(separator);
+            vscode.env.clipboard.writeText(clipboard);
+            vscode.window.showInformationMessage('Path copied to clipboard');
         }
     });
     context.subscriptions.push(disposable);
--- src/extension.ts
+++ src/extension.ts
@@ -7,52 +7,60 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('pytest-grabber.generatePath', () => {
 		const editor = vscode.window.activeTextEditor;
 
-		if (editor) {
-			const config = vscode.workspace.getConfiguration('pytest-grabber');
-			const separator = config.get<string>('separator') || '${separator}';
+		if (!editor) return;  // Early return if no editor is active
 
-			const prefixCmd = config.get<string>('prefixCmd') || '';
+		const config = vscode.workspace.getConfiguration('pytest-grabber');
+		const document = editor.document;
+		const selection = editor.selection;
 
-			const document = editor.document;
-			const selection = editor.selection;
+		let classNameMatch: RegExpMatchArray | null = null;
+		let methodNameMatch: RegExpMatchArray | null = null;
 
-			const line = document.lineAt(selection.active.line);
-			let classNameMatch: RegExpMatchArray | null = null;
-			let methodNameMatch: RegExpMatchArray | null = null;
+		// Search for the class and method names above the current line
+		for (let i = selection.active.line; i >= 0; i--) {
+			const line = document.lineAt(i);
 
-			// Search for the class and method names above the current line
-			for (let i = selection.active.line; i >= 0; i--) {
-				const line = document.lineAt(i);
+			if (!methodNameMatch) methodNameMatch = line.text.match(/def\s+(\w+)\(/);
 
-				if (!methodNameMatch) {
-					methodNameMatch = line.text.match(/def\s+(\w+)\(/);
-				}
 
-				if (!classNameMatch) {
-					classNameMatch = line.text.match(/class\s+(\w+)([\(:])/);
-				}
+			if (!classNameMatch) classNameMatch = line.text.match(/class\s+(\w+)([\(:])/);
 
-				if (classNameMatch) {
-					break;
-				}
+			if (classNameMatch) break;
+		}
+
+		// Gather all path components.
+		const separator = config.get<string>('separator') || '${separator}';
+		// Get prefixCmd and add a space if it doesn't have it.
+		let prefixCmd = config.get<string>('prefixCmd') || '';
+		prefixCmd = prefixCmd.endsWith(" ") ? prefixCmd : prefixCmd + " ";
+		const filePath = vscode.workspace.asRelativePath(document.uri, false);
+		const className = classNameMatch?.[1];
+		const methodName = methodNameMatch?.[1];
+
+		if (!className && !methodName) {
+			vscode.window.showInformationMessage('No match found!');
+		} else {
+			// We have at least one part.
+			// Gather all path components in an array for simplicity
+			const pathComponents: string[] = [];
+
+			pathComponents.push(filePath);
+
+			if (className && methodName) {
+				pathComponents.push(className, methodName);
+			} else if (className) {
+				pathComponents.push(className);
+			} else if (methodName) {
+				pathComponents.push(methodName);
 			}
 
-			if (classNameMatch && methodNameMatch) {
-				const className = classNameMatch[1];
-				const methodName = methodNameMatch[1];
-				const filePath = vscode.workspace.asRelativePath(document.uri, false);
+			let clipboard = "";
+			if (prefixCmd) clipboard += prefixCmd + " ";
 
-				vscode.env.clipboard.writeText(`${prefixCmd}${filePath}${separator}${className}${separator}${methodName}`);
-				vscode.window.showInformationMessage('Path copied to clipboard');
-			} else if (classNameMatch) {
-				const className = classNameMatch[1];
-				const filePath = vscode.workspace.asRelativePath(document.uri, false);
+			clipboard += pathComponents.join(separator);
 
-				vscode.env.clipboard.writeText(`${prefixCmd}${filePath}${separator}${className}`);
-				vscode.window.showInformationMessage('Path copied to clipboard');
-			} else {
-				vscode.window.showInformationMessage('No match found!');
-			}
+			vscode.env.clipboard.writeText(clipboard);
+			vscode.window.showInformationMessage('Path copied to clipboard');
 		}
 	});
 
